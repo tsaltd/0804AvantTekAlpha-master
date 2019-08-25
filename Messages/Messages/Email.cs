dee73c3c4b6ab0556774740df5c8edc0097e0947
S. D. Mintz <wp710012@gmail.com>
2019-08-25 15:56:28 -0400

    Add project files.

---
+++ Messages/Messages/Email.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Messages
+{
+    // This project can output the Class library as a NuGet Package.
+    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+    public class Email
+    {
+       
+        public string To { get; set; }
+        public string From { get; set; }
+        public string Subject { get; set; }
+        public string Text { get; set; }
+
+    }
+}
